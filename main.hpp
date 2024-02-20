484323d88e162a51b2db0458059ca9f53ea26f6a
Kyu Lee <kwooler@gmail.com>
2024-02-19 20:59:36 -0800

    A902

--- main.hpp
+++ main.hpp
@@ -18,41 +18,12 @@ void printStudent(Student *head);
 
 Student *makeStudent()
 {
-    Student *head, *tmp, *prev;
-    ifstream ifs;
-    ifs.open("students.txt");
-    if (!ifs)
-    {
-        cerr << "File Open Error\n";
-        exit(0);
-    }
-    for (int i = 0; i < N; i++)
-    {
-        tmp = (Student *)malloc(sizeof(Student));
-        ifs >> tmp->id >> tmp->name >> tmp->major;
-        tmp->next = NULL;
-        if (i == 0)
-            head = tmp;
-        else
-            prev->next = tmp;
-        prev = tmp;
-    }
-    return head;
     /*******************************
      * Code your program here
      *******************************/
 }
 void printStudent(Student *head)
 {
-    Student *tmp;
-    tmp = head;
-    while (tmp != NULL)
-    {
-        cout << tmp->id << "\t";
-        cout << tmp->name << "\t";
-        cout << tmp->major << "\n";
-        tmp = tmp->next;
-    }
     /*******************************
      * Code your program here
      *******************************/
