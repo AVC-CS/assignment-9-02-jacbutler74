be762225fa8f5b4ee1e85c5933a7fd0c1b3038b4
Kyuwoong Lee <kwooler@gmail.com>
2024-02-19 20:38:22 -0800

    Initial commit

---
+++ main.hpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+using namespace std;
+
+const int N = 5;
+struct Student
+{
+    int id;
+    string name;
+    string major;
+};
+
+void makeStudent();
+void printStudent(Student *head);
+
+void makeStudent(Student *student)
+{
+    /*******************************
+     * Code your program here
+     *******************************/
+}
+void printStudent(Student *student)
+{
+    cout << endl;
+    /*******************************
+     * Code your program here
+     *******************************/
+}
